1cd1f8bd4db25b66ec766262f1d64a7a5ed05e98
Mostafa Elaasr <196775809+amrMohamed-pureSoft@users.noreply.github.com>
2025-09-27 00:16:38 +0300

    edit home images

---
+++ lib/core/masarate_assets.dart
@@ -0,0 +1,176 @@
+// Auto-generated class containing lists of image assets from assets/masarat
+
+class MasaratAssets {
+  static const List<String> altanmia = [
+    "assets/masarat/التنمية/Capture.PNG",
+    "assets/masarat/التنمية/ed1fa16a-2721-4fb9-b9b1-b54dfb118df1-962.png",
+  ];
+
+  static const List<String> alsodoor = [
+    "assets/masarat/الصدور/4868979d-ff91-4e74-92ae-0e265851ca85.png",
+    "assets/masarat/الصدور/9590-01-opt_1.png",
+    "assets/masarat/الصدور/default.png",
+    "assets/masarat/الصدور/zamil.png",
+  ];
+
+  static const List<String> alwatanya = [
+    "assets/masarat/الوطنية/دجاج وطنية 1100.png",
+    "assets/masarat/الوطنية/دجاج وطنية800.png",
+    "assets/masarat/الوطنية/وطنية1000.png",
+    "assets/masarat/الوطنية/وطنية600.PNG",
+    "assets/masarat/الوطنية/وطنية700.PNG",
+  ];
+
+  static const List<String> khodraat = [
+    "assets/masarat/خضراوات/5.1.1.1-Seara-French-Fries-1-KG-Front.png",
+    "assets/masarat/خضراوات/6223001751007-1.png",
+    "assets/masarat/خضراوات/Dari-origianl-fries-2kg.png",
+    "assets/masarat/خضراوات/e53cad8f-1286-410f-805e-75d572bf6c73-1000x1000-k9EtlJoJD0dorPlHajmHYrcW6IpCCrRMWE8p2sS2.png",
+  ];
+
+  static const List<String> dajajDox = [
+    "assets/masarat/دجاج دوكس/01J18KYDC3PJHJGM8AF7CMXSYA.png",
+  ];
+
+  static const List<String> dajajSadia = [
+    "assets/masarat/دجاج ساديا/4140_sadia-chicken-1000-gm_1.png",
+    "assets/masarat/دجاج ساديا/6120_11294_saudia-chicken-1100-gm_1.png",
+    "assets/masarat/دجاج ساديا/7909_11307_saudia-chicken-900-gm_1.png",
+    "assets/masarat/دجاج ساديا/sadia.png",
+  ];
+
+  static const List<String> dajajFaqih = [
+    "assets/masarat/دجاج فقيه/Alfakih-chicken-1000g.png",
+    "assets/masarat/دجاج فقيه/Alfakih-chicken-1100g.png",
+    "assets/masarat/دجاج فقيه/Alfakih-chicken-800g.png",
+    "assets/masarat/دجاج فقيه/fakih600.png",
+    "assets/masarat/دجاج فقيه/fakih700.png",
+  ];
+
+  static const List<String> radwa = [
+    "assets/masarat/رضوى/0105410_1.png",
+    "assets/masarat/رضوى/0105422_1.png",
+    "assets/masarat/رضوى/0105435_1.png",
+    "assets/masarat/رضوى/0105448_1.png",
+    "assets/masarat/رضوى/Al-Kabeer.png",
+  ];
+
+  static const List<String> zoyot = [
+    "assets/masarat/زيوت/Afia_Corn_Oil_5L.png",
+    "assets/masarat/زيوت/ayn.png",
+    "assets/masarat/زيوت/corn.png",
+    "assets/masarat/زيوت/nakhla.png",
+  ];
+
+  static const List<String> shawarmaDajaj = [
+    "assets/masarat/شاورما الدجاج/Al-Kabeer-Chicken-Shawarma-1kg-1.png",
+  ];
+
+  static const List<String> kebda = [
+    "assets/masarat/كبده/Al-Kabeer-Frozen-Chicken-Liver-450g-1.png",
+  ];
+
+  static const List<String> kwalikwa = [
+    "assets/masarat/كواليكوا/kwalikwa.png",
+  ];
+
+  static const List<String> lo7oom = [
+    "assets/masarat/لحوم/Al-Kabeer-Beef-Burger-1kg-1.png",
+    "assets/masarat/لحوم/Al-Kabeer-Mutton-Kofta-260g.png",
+    "assets/masarat/لحوم/Al-Kabeer-Veal-Sausage-340g.png",
+  ];
+
+  static const List<String> mafrumDajaj = [
+    "assets/masarat/مفروم الدجاج/chicken-mince-al-kabeer-400g.png",
+  ];
+
+  static const List<String> maqataatDajaj = [
+    "assets/masarat/مقطعات دجاج/Al-Kabeer-Bone-Less-Chicken-Breast-2kg-1.png",
+    "assets/masarat/مقطعات دجاج/Al-Kabeer-Chicken-Drumsticks-900g-1.png",
+    "assets/masarat/مقطعات دجاج/Al-Kabeer-Chicken-Griller-1100g-1.png",
+    "assets/masarat/مقطعات دجاج/Al-Kabeer-Chicken-Wings-900g.png",
+  ];
+
+  static const List<String> montagatWara2yaWblastikia = [
+    "assets/masarat/منتجات ورقية وبلاستيكية/family.png",
+    "assets/masarat/منتجات ورقية وبلاستيكية/plastic.png",
+    "assets/masarat/منتجات ورقية وبلاستيكية/tissue.png",
+  ];
+
+  static const List<String> mowadGafaWsa2la = [
+    "assets/masarat/مواد جافة وسائلة/Al-Kabeer-Frozen-Green-Peas-400g.png",
+    "assets/masarat/مواد جافة وسائلة/Al-Kabeer-Frozen-Molokhia-400g.png",
+    "assets/masarat/مواد جافة وسائلة/Al-Kabeer-Frozen-Okra-400g.png",
+  ];
+}
+// Auto-generated list containing all image assets from assets/masarat
+
+const List<String> masaratAll = [
+  "assets/masarat/التنمية/Capture.PNG",
+  "assets/masarat/التنمية/ed1fa16a-2721-4fb9-b9b1-b54dfb118df1-962.png",
+
+  "assets/masarat/الصدور/4868979d-ff91-4e74-92ae-0e265851ca85.png",
+  "assets/masarat/الصدور/9590-01-opt_1.png",
+  "assets/masarat/الصدور/default.png",
+  "assets/masarat/الصدور/zamil.png",
+
+  "assets/masarat/الوطنية/دجاج وطنية 1100.png",
+  "assets/masarat/الوطنية/دجاج وطنية800.png",
+  "assets/masarat/الوطنية/وطنية1000.png",
+  "assets/masarat/الوطنية/وطنية600.PNG",
+  "assets/masarat/الوطنية/وطنية700.PNG",
+
+  "assets/masarat/خضراوات/5.1.1.1-Seara-French-Fries-1-KG-Front.png",
+  "assets/masarat/خضراوات/6223001751007-1.png",
+  "assets/masarat/خضراوات/Dari-origianl-fries-2kg.png",
+  "assets/masarat/خضراوات/e53cad8f-1286-410f-805e-75d572bf6c73-1000x1000-k9EtlJoJD0dorPlHajmHYrcW6IpCCrRMWE8p2sS2.png",
+
+  "assets/masarat/دجاج دوكس/01J18KYDC3PJHJGM8AF7CMXSYA.png",
+
+  "assets/masarat/دجاج ساديا/4140_sadia-chicken-1000-gm_1.png",
+  "assets/masarat/دجاج ساديا/6120_11294_saudia-chicken-1100-gm_1.png",
+  "assets/masarat/دجاج ساديا/7909_11307_saudia-chicken-900-gm_1.png",
+  "assets/masarat/دجاج ساديا/sadia.png",
+
+  "assets/masarat/دجاج فقيه/Alfakih-chicken-1000g.png",
+  "assets/masarat/دجاج فقيه/Alfakih-chicken-1100g.png",
+  "assets/masarat/دجاج فقيه/Alfakih-chicken-800g.png",
+  "assets/masarat/دجاج فقيه/fakih600.png",
+  "assets/masarat/دجاج فقيه/fakih700.png",
+
+  "assets/masarat/رضوى/0105410_1.png",
+  "assets/masarat/رضوى/0105422_1.png",
+  "assets/masarat/رضوى/0105435_1.png",
+  "assets/masarat/رضوى/0105448_1.png",
+  "assets/masarat/رضوى/Al-Kabeer.png",
+
+  "assets/masarat/زيوت/Afia_Corn_Oil_5L.png",
+  "assets/masarat/زيوت/ayn.png",
+  "assets/masarat/زيوت/corn.png",
+  "assets/masarat/زيوت/nakhla.png",
+
+  "assets/masarat/شاورما الدجاج/Al-Kabeer-Chicken-Shawarma-1kg-1.png",
+
+  "assets/masarat/كبده/Al-Kabeer-Frozen-Chicken-Liver-450g-1.png",
+
+  "assets/masarat/كواليكوا/kwalikwa.png",
+
+  "assets/masarat/لحوم/Al-Kabeer-Beef-Burger-1kg-1.png",
+  "assets/masarat/لحوم/Al-Kabeer-Mutton-Kofta-260g.png",
+  "assets/masarat/لحوم/Al-Kabeer-Veal-Sausage-340g.png",
+
+  "assets/masarat/مفروم الدجاج/chicken-mince-al-kabeer-400g.png",
+
+  "assets/masarat/مقطعات دجاج/Al-Kabeer-Bone-Less-Chicken-Breast-2kg-1.png",
+  "assets/masarat/مقطعات دجاج/Al-Kabeer-Chicken-Drumsticks-900g-1.png",
+  "assets/masarat/مقطعات دجاج/Al-Kabeer-Chicken-Griller-1100g-1.png",
+  "assets/masarat/مقطعات دجاج/Al-Kabeer-Chicken-Wings-900g.png",
+
+  "assets/masarat/منتجات ورقية وبلاستيكية/family.png",
+  "assets/masarat/منتجات ورقية وبلاستيكية/plastic.png",
+  "assets/masarat/منتجات ورقية وبلاستيكية/tissue.png",
+
+  "assets/masarat/مواد جافة وسائلة/Al-Kabeer-Frozen-Green-Peas-400g.png",
+  "assets/masarat/مواد جافة وسائلة/Al-Kabeer-Frozen-Molokhia-400g.png",
+  "assets/masarat/مواد جافة وسائلة/Al-Kabeer-Frozen-Okra-400g.png",
+];
